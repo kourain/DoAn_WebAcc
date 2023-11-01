85ce333d409481c699a73f7973109290dfd6e88d
Kourain <maiquyen16503@gmail.com>
2023-11-01 13:41:32 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoAn_WebAcc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
